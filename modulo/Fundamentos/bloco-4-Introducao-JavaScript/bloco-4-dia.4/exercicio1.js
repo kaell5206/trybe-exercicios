ed05d8c6163d940ac37e5df9b55002ce0baf3b1a
KaellAlec <kaellalecs@gmail.com>
2021-12-02 09:14:57 -0300

    exercicios dia 4.4

---
+++ modulo/Fundamentos/bloco-4-Introducao-JavaScript/bloco-4-dia.4/exercicio1.js
@@ -0,0 +1,23 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrent: 'yes',
+  };
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota:  'O último MacPatinhas',
+    recorrent: 'yes',
+  };
+  console.log(info.personagem+" "+"e "+info2.personagem)
+  console.log(info.origem + ' '+'e '+info2.origem)
+  console.log(info.nota+' '+'e '+info2.nota)
+  console.log(info.recorrent+' '+'e '+info2.recorrent)
+
+
+
+
+  //   for (key in info) {
+//       console.log(info[key])
+//   }
