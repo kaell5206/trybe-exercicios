4be13fdad80b90bd0bcfcf62f4f7bf064f2ebb43
KaellAlec <kaellalecs@gmail.com>
2022-01-19 11:26:57 -0300

    exercicio 2

--- modulo/Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ modulo/Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,14 +1,31 @@
-const newEmployees = (newObj) => {
-  const employees = {
-    id1: newObj('Kaell Caldeira'),
-    id2: newObj('Clara Medeiros'),
-    id3: newObj('Fernando Bissi'),
-  }
-  return employees;
-};
+// EXER 1
+// const newEmployees = (newObj) => {
+//   const employees = {
+//     id1: newObj('Kaell Caldeira'),
+//     id2: newObj('Clara Medeiros'),
+//     id3: newObj('Fernando Bissi'),
+//   }
+//   return employees;
+// };
 
-const newObj = (fullName) => {
-  const obj = {nomeCompleto: fullName, email: `${fullName.split(' ').join('_').toLowerCase()}@trybe.com`,}
-  return obj;
+// const newObj = (fullName) => {
+//   const obj = {nomeCompleto: fullName, email: `${fullName.split(' ').join('_').toLowerCase()}@trybe.com`,}
+//   return obj;
+// }
+// console.log(newEmployees(newObj))
+const sortear = (numero, func) => {
+  let sorteado = Math.round(Math.random() * (5 - 1) + 1);
+  let result = func(numero, sorteado);
+  return result;
 }
-console.log(newEmployees(newObj))
+
+const compara = (numero, resultado) => {
+  if (resultado === numero) {
+    return `Você ganhou, você escolheu ${numero} e o sorteado é ${resultado}`
+  } else {
+    return `Tente novamente. o numero era ${resultado} e você escolheu ${numero}`
+  }
+}
+
+
+console.log(sortear(3, compara));
