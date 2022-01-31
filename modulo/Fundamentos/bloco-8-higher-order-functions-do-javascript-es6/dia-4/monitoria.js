90c644bf256f405dc8bbe2bf4b993766c6665218
KaellAlec <kaellalecs@gmail.com>
2022-01-31 12:52:59 -0300

    stuff

--- modulo/Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4/monitoria.js
+++ modulo/Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4/monitoria.js
@@ -72,3 +72,51 @@ function totalVotes(arr) {
 //   }
   
 //   console.log()
+
+// Faça uma lista com as suas frutas favoritas
+// const specialFruit = ['Banana', 'Pêra', 'uva'];
+
+// // Faça uma lista de complementos que você gostaria de adicionar
+// const additionalItens = ['Castanha', 'Leite em pó', 'Nutella'];
+
+// const fruitSalad = (fruit, additional) => {
+//   // Esreva sua função aqui
+//   return [...fruit,...additional]
+// };
+
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const contratado = {
+  ...user,
+  ...jobInfos,
+}
+console.log(contratado)
+
+
+const test = ({ name, age, nationality, profession, squad, squadInitials}) => {
+ console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`)
+}
+test(contratado)
+
+// const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+// // saudacoes[1](saudacoes[0]); // Olá
+
+// // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+// const [primeiro, segundo] = saudacoes;
+
+// segundo(primeiro))
